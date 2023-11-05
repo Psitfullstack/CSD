417c01c5539cd35806be87949da6a0bb2d5c968a
Amir Khan <amirkhan1985@gmail.com>
2023-11-05 23:51:04 +0530

    update

---
+++ src/components/Nav2.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Nav2.css'
+export default function Nav2() {
+  return (
+    <div className='container2'> 
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Service</a></li>
+        </ul>
+     </nav>
+
+    </div>
+  )
+}
