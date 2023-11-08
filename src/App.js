a6b12dedb129ea2432065ae169d157a3b6589ef7
Amir Khan <amirkhan1985@gmail.com>
2023-11-08 11:08:09 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav'
-
+import Funcomp from './components/Funcomp'
 import Home from './components/Home'
 
 function App() {
+  const obj1={name:'rohan', age:20}
   return (
     <>
      <Nav/>
      <Home/>
+     <Funcomp {...obj1}  />
      </>
   );
 }
---
+++ src/components/Funcomp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Funcomp(props) {
+  return (
+    <>
+    <div>{props.name}</div>
+    <div>{props.age}</div>
+    </>
+  )
+}
